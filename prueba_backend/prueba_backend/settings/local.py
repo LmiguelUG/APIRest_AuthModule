42ed2f56ab4315ab6d58809d4d1bde7f82b4ebd0
lmiguelug <luism3872@gmail.com>
2021-08-12 12:54:08 -0400

    postgresql configuration as database

--- prueba_backend/prueba_backend/settings/local.py
+++ prueba_backend/prueba_backend/settings/local.py
@@ -10,7 +10,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_prueba',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
