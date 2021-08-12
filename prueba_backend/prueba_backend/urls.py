08d35e8e3c1541f6800e4a0de6dad3240c367f5d
lmiguelug <luism3872@gmail.com>
2021-08-12 16:36:07 -0400

    'views' and 'urls' for user CRUD

--- prueba_backend/prueba_backend/urls.py
+++ prueba_backend/prueba_backend/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # CRUD users
+    path('user/', include('apps.users.api_users.urls')), # Ruta para CRUD de usuarios
 ]
